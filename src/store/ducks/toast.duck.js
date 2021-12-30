76166df1172c084d81b18e4d44d30731bbabf11b
Denis Slipko <denisslipko@MacBook-Pro-Denis.local>
2021-12-30 21:39:08 +0200

    fix typo

--- src/store/ducks/toast.duck.js
+++ src/store/ducks/toast.duck.js
@@ -1,15 +1,15 @@
 import { put, delay } from 'redux-saga/effects';
 import { setSuccessToast, setFailureToast, setDefaultToast } from '../actions/toast.actions';
 
-const defaultDelay = 2000;
+const DefaultDelay = 2000;
 
-export function* getSuccessToast(message, delayTime = defaultDelay) {
+export function* getSuccessToast(message, delayTime = DefaultDelay) {
   yield put(setSuccessToast('success', message, true));
   yield delay(delayTime);
   yield put(setDefaultToast());
 }
 
-export function* getFailureToast(error, delayTime = defaultDelay) {
+export function* getFailureToast(error, delayTime = DefaultDelay) {
   yield put(setFailureToast('danger', error, true));
   yield delay(delayTime);
   yield put(setDefaultToast());
