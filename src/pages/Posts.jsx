fe86035e9775bef6db68db8d5eb2c020bd7ef3f5
Denis Slipko <denisslipko@MacBook-Pro-Denis.local>
2022-01-11 18:25:21 +0200

    fix logic in Posts

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -6,15 +6,23 @@ import { fetchCities } from '../store/actions/cities.actions';
 import Card from '../components/Card/Card';
 import Pagination from '../components/Pagination/Pagination';
 import { DEFAULT_AMOUNT_EL } from '../constants/constants'
+import { useLocation } from 'react-router-dom';
 
 const Posts = () => {
   const dispatch = useDispatch();
   
   const cities = useSelector(getCities);
   const totalAmount = useSelector(getTotalAmount);
+
+  const location = useLocation();
+
+  const searchParams = new URLSearchParams(location.search);
+
+  const amount = searchParams.get('amount') || DEFAULT_AMOUNT_EL;
+  const page = searchParams.get('page') || 1;
   
-  const [currentPage, setCurrentPage] = useState(1);
-  const [amountElOnPage, setAmountElOnPage] = useState(DEFAULT_AMOUNT_EL);
+  const [currentPage, setCurrentPage] = useState(page);
+  const [amountElOnPage, setAmountElOnPage] = useState(amount);
   
   const pagesAmount = Math.ceil(totalAmount / amountElOnPage);
   
@@ -35,7 +43,11 @@ const Posts = () => {
         <Card key={id} text={state_name} title={name} />
       ))}
     </div>
-      <Pagination pagesAmount={pagesAmount} onPageChange={setCurrentPage} onChangeAmountEl={handleChangeAmountEl} />
+      <Pagination 
+        pagesAmount={pagesAmount} 
+        searchParams={searchParams} 
+        onPageChange={setCurrentPage} 
+        onChangeAmountEl={handleChangeAmountEl} />
     </>
   );
 };
